e369b85316ba88d7f8cef83297e2066bf797deab
koda <hogehogehoge@gmail.com>
2019-06-16 19:49:59 +0900

    ラベルフィルターの追加

--- my-project/src/store.js
+++ my-project/src/store.js
@@ -39,6 +39,19 @@ export default new Vuex.Store({
     nextTaskId:3,
     nextLabelId: 4,
 
+    filter: null
+
+  },
+
+  getters:{
+    filteredTasks(state){
+      if(!state.filter){
+        return state.tasks
+      }
+      return state.tasks.filter(task => {
+        return task.labelIds.indexOf(state.filter) >= 0
+      })
+    }
   },
   mutations: {
     // タスクを追加
@@ -70,6 +83,10 @@ export default new Vuex.Store({
       })
       // 次に追加されるラベルに付与するIDを更新する
       state.nextLabelId++
+    },
+
+    changeFilter(state,{ filter }){
+      state.filter = filter
     },
   },
   //actions: {
